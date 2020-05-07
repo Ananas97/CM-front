3d721c3931bcaa3ec5513183c35a596b07b05cac
Ananas <ana.rei.014@gmail.com>
2020-05-07 20:20:57 +0200

    edit files, trying to get method working

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
+const router = express.Router();
+app.use('/*', router);
+const path = require('path');
 
 if(process.env.NODE_ENV === 'production'){
-  //set static folder
-  app.use(express.static('/dist/index.html'));
+  app.use(express.static(__dirname + '/dist'));
 }
-
 
 app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
+
+//routing
+// router.get('/*', function (req, res) {
+//   res.sendFile(path.join(__dirname + '/dist/index.html'));
+// });
+
+console.log("it works!");
