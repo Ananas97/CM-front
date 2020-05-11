3b58cdef4f6a40ae21f27e0f5a92d9e3e70e536a
Ananas <ana.rei.014@gmail.com>
2020-05-11 19:20:45 +0200

    update assertions in tests to pass

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 
 if(process.env.NODE_ENV === 'production'){
   //set static folder
-  app.use(express.static('src/index.html'));
+  app.use(express.static('/dist/web-app/index.html'));
 }
 
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/we-app'));
 app.listen(process.env.PORT || 8080);
